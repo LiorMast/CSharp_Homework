3f38900a7ebfcdca5de95334e6e841aed167c0bc
Lior Masturov <liormasturov@gmail.com>
2023-03-19 22:42:55 +0200

    added comments

--- Question 3/Program.cs
+++ Question 3/Program.cs
@@ -12,26 +12,27 @@ namespace Question_3
         {
             int[] arr = { -1, 0, 1, 2, -1, 4, -3 };
 
-            int[,] arr2 = new int[arr.Length* arr.Length* arr.Length, 3];
+            int[,] arr2 = new int[arr.Length* arr.Length* arr.Length, 3]; //Create two-dim array to hold all the triplets
             int count = 0;
 
             for (int i = 0; i < arr.Length; i++)
                 for (int j = i+1; j < arr.Length; j++)
-                    for (int k = j+1; k < arr.Length; k++)
+                    for (int k = j+1; k < arr.Length; k++)  // Nested loops iterate through all possible combinations of three integers from the "arr" array
                     {
-                        if (arr[i] + arr[j] + arr[k]==0)
-                        
-                            {
+                        if (arr[i] + arr[j] + arr[k]==0) // If the sum of three integers is equal to zero, store those integers in the "arr2" array and increment the "count" variable
+
+                        {
                                 arr2[count,0] = arr[i];
                                 arr2[count, 1] = arr[j];
                                 arr2[count, 2] = arr[k];
                                 count++;
                             }
                     }
-            arr2 = resize2dArray(arr2, count, 3);
-            print2dArr(arr2);
+            arr2 = resize2dArray(arr2, count, 3); // Resize the "arr2" array to the exact size needed to fit all the valid combinations found
+            print2dArr(arr2); // Print out the valid combinations of three integers whose sum is zero
         }
 
+        // Method to print out a two-dimensional integer array
         static void print2dArr(int[,] arr)
         {
             int count = 0;
@@ -39,7 +40,7 @@ namespace Question_3
             {
                 Console.Write($"{i}\t");
                 count++;
-                if (count == arr.GetLength(1))
+                if (count == arr.GetLength(1)) //Drop down a line when you finish a row
                 {
                     Console.WriteLine("");
                     count = 0;
@@ -47,17 +48,18 @@ namespace Question_3
             }
         }
 
+        // Method to resize a two-dimensional integer array
         static int[,] resize2dArray(int[,] arr, int row, int col)
         {
-            int[,] newarr = new int[row, col];
+            int[,] newarr = new int[row, col];  // Create a new two-dimensional integer array with the dimensions of (row, col)
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < col; j++)
                 {
-                    newarr[i, j] = arr[i, j];
+                    newarr[i, j] = arr[i, j]; // Copy the valid combinations from the old "arr2" array to the new "newarr" array
                 }
             }
-            return newarr;
+            return newarr; // Return the new two-dimensional integer array
         }
     }
 }
