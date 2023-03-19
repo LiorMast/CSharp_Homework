f68b8e6e89ff1e76635555a824d53f093bfae452
Lior Masturov <liormasturov@gmail.com>
2023-03-19 22:49:39 +0200

    added comments

--- C#_Homework/Program.cs
+++ C#_Homework/Program.cs
@@ -11,30 +11,33 @@ namespace C__Homework
         static void Main(string[] args)
         {
             int[] nums = new int[9];
-            Console.WriteLine("Please enter 9 numbers: ");
+            Console.WriteLine("Please enter 9 numbers: "); // Ask the user to enter 9 integers
             for (int i = 0; i < nums.Length; i++)
             {
                 nums[i] = int.Parse(Console.ReadLine());
             }
 
-            Array.Sort(nums);
-            int[] distinct = new int[nums.Length];
+            Array.Sort(nums); // Sort the array in ascending order
+
+            int[] distinct = new int[nums.Length]; // Create a new array to store the distinct values
             int j = 0;
 
-            for (int i = 0; i < nums.Length; ++i)
+            for (int i = 0; i < nums.Length; ++i) // Loop through the sorted array and find the last index of each distinct value
             {
                 int res = Array.FindLastIndex(nums, x => x == nums[i]);
-                if (res == i)
+
+                if (res == i) // If the last index of a value is equal to the current index, it is distinct
                 {
                     distinct[j++] = nums[i];
                 }
-                else
+                else // Otherwise, skip over the values that are the same
                 {
                     i = res;
                 }
             }
-            Array.Resize(ref distinct, j);
-            Console.Write("[");
+            Array.Resize(ref distinct, j); // Resize the distinct array to only include the distinct values
+
+            Console.Write("["); // Print out the distinct values
             foreach (int i in distinct)
             {
                 Console.Write($"{i}, ");
--- Question 1/Program.cs
+++ Question 1/Program.cs
@@ -10,17 +10,25 @@ namespace Question_1
     {
         static void Main(string[] args)
         {
-            const double rate = 3.42;
-            string workername = "Joe";
-            Console.WriteLine($"Welcome to {workername}'s Store.\n");
-            Console.Write("Please enter your name: ");
+            const double rate = 3.42;  // set the exchange rate
+
+            string workername = "Joe"; // set the name of the store worker
+
+            Console.WriteLine($"Welcome to {workername}'s Store.\n"); // welcome the user to the store
+
+            Console.Write("Please enter your name: "); // ask the user for their name
             string custname = Console.ReadLine();
-            Console.Write($"Hello {custname}, What is the amount of dollars you would like to recieve? ");
+
+            Console.Write($"Hello {custname}, What is the amount of dollars you would like to recieve? "); // ask the user for the amount of dollars they want to exchange
             double amount = double.Parse(Console.ReadLine());
+
+            // calculate the amount in shekels, the fee, the total price, and the fee payment
             double amountInShekels = amount * rate;
             double fee = amountInShekels * (15.0 / 1000);
             double total = amountInShekels + fee;
             double feePayment = fee / 3;
+
+            // print the final invoice to the user
             Console.WriteLine($"Here's your final invoice, {custname}:\n\t#Amount of dollars: {amount}\n\t#Price in shekels: {amountInShekels}\n\t#Fee in one payment: {fee}\n\t#Fee in three payments: {feePayment}\n\t#Total price: {total}");
         }
     }
